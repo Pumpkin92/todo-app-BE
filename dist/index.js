36e209fee243321d6514be17483ae9779d6349f6
Pumpkin92 <77683675+Pumpkin92@users.noreply.github.com>
2024-06-20 22:12:25 +0100

    updates port info

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const apiRouter = require("./routers/api.router");
 const app = (0, express_1.default)();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use("/", apiRouter);
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 
 const apiRouter = require("./routers/api.router");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
